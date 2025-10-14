6043b8ff40f402697c485f91fc0bdc3f0b71d8df
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-10-14 11:32:24 +0500

    my-app

---
+++ app.js
@@ -0,0 +1,40 @@
+// Elements
+let password = document.getElementById("password")
+let copyBtn = document.getElementById("copyBtn")
+let changIcon = document.getElementById("changIcon")
+let txt = document.getElementById("txt")
+
+// Password Handling
+password.addEventListener("input" , () => {
+
+    if (password.value.length === 0) {
+        txt.innerText = ""
+    }
+    else if (password.value.length <= 4) {
+        txt.innerText = "Password is weak"
+        txt.style.color = "#ff5925"
+    }
+    else if (password.value.length <= 8) {
+        txt.innerText = "Password is medium"
+        txt.style.color = "yellow"
+    }
+    else {
+        txt.innerText = "Password is strong"
+        txt.style.color = "#26d730"
+    }
+
+})
+
+// Icon Handling
+changIcon.addEventListener("click" , () => {
+
+    if (changIcon.classList.contains("fa-eye")) {
+        changIcon.className = "fa-regular fa-eye-slash hover:cursor-pointer"
+        password.type = "password"
+    }
+    else {
+        password.type = "text"
+        changIcon.className = "fa-regular fa-eye hover:cursor-pointer"
+    }
+    
+})
